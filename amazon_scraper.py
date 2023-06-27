981b49c9dcf7ade7a741217839c0d027c05ee9b2
adamruehle <adamruehle@live.com>
2023-06-27 18:00:04 -0400

    Introduced Func in amazon_scraper to Find a Specified Item on the Search Page

--- amazon_scraper.py
+++ amazon_scraper.py
@@ -11,13 +11,31 @@ import requests
 import time
 import datetime
 
-def scrape_amazon_data(product_name):
-  options = webdriver.ChromeOptions()
-  # options.add_argument("--headless")
-  driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+def use_search_bar(driver, product_name):
   driver.get("https://www.amazon.com/")
   wait = WebDriverWait(driver, 10)
   search_input = wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "input[id='twotabsearchtextbox']")))
   search_input.send_keys(product_name)
   search_input.submit()
   print("Current URL:", driver.current_url)
+
+def find_product_at_index(driver, index):
+  try:
+    product_elements = driver.find_elements("css selector", "div[data-component-type='s-search-result']")
+    if index < len(product_elements):
+      product_element = product_elements[index]
+      link_element = product_element.find_element(By.CSS_SELECTOR, "h2 a")
+      link_url = link_element.get_attribute("href")
+      print("Link URL:", link_url)
+    else:
+      print("Product index is out of range.")
+  except Exception as e:
+      print("Error occurred:", str(e))
+
+def initialize_scraper(product_name):
+  options = webdriver.ChromeOptions()
+  # options.add_argument("--headless")
+  driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+  use_search_bar(driver, product_name)
+  find_product_at_index(driver, 0)
+  driver.quit()
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import amazon_scraper
 
 def main():
   product_name = input("Enter a product to scrape: ")
-  amazon_scraper.scrape_amazon_data(product_name)
+  amazon_scraper.initialize_scraper(product_name)
 
 if __name__ == "__main__":
   main()
